f58111f76d9b8c2931f41af26f67d8d038bd6593
Paul G <paulsystem32@gmail.com>
2020-08-01 00:47:34 -0600

    Fin de BasicoPage
    - Se finalizo BasicoPage, ahora se continuara con el ScrollPage.
    - En este commit se implemento la pagina Scrollpage.

---
+++ lib/pages/scroll_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Scrollpage extends StatelessWidget {
+  const Scrollpage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Scroll page'),
+    );
+  }
+}
