ae5df9762382fd5451971a65bf35853738cd12dc
Paul G <paulsystem32@gmail.com>
2020-08-01 01:09:41 -0600

    ScrollPage: Implementacion de Stack
    - Se implemento el Stack para mostrar los elementos dentro del pageView
      apilados y bien ordenados.

--- lib/pages/scroll_page.dart
+++ lib/pages/scroll_page.dart
@@ -17,13 +17,52 @@ class Scrollpage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
   }
+
   Widget _pagina2() {
     return Center(
       child: Text('pagina 2'),
+    );
+  }
+
+  Widget _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          Text('11°', style: estiloTexto),
+          Text('Miercoles', style: estiloTexto),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white),
+        ],
+      ),
     );
   }
 }
