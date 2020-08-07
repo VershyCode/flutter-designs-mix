509d4c8d0e9c3a0f85f7fa7a61a0502e3f5c1da7
Paul G <paulsystem32@gmail.com>
2020-08-06 23:15:43 -0600

    Cambio de color en el Status bar
    - Se cambio de color la status bar a blanco.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:designs/pages/basico_page.dart';
 import 'package:designs/pages/botones_page.dart';
 import 'package:designs/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.white
+    ));
+
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
